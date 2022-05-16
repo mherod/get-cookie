b0d1d725db33c45c5fbda15105ee6914128fc364
Matthew Herod <matthew.herod@myunidays.com>
2022-05-16 13:39:46 +0100

    Works with Firefox cookies too

--- index.js
+++ index.js
@@ -27,8 +27,68 @@ async function getChromePassword() {
     });
 }
 
+/**
+ *
+ * @param name
+ * @param domain
+ * @returns {Promise<Buffer>}
+ */
+async function getFirefoxCookie({name, domain}) {
+    if (name && typeof name !== 'string') {
+        throw new Error('name must be a string');
+    }
+    if (domain && typeof domain !== 'string') {
+        throw new Error('domain must be a string');
+    }
+    return await new Promise((resolve, reject) => {
+        const file = `${process.env.HOME}/Library/Application Support/Firefox/Profiles/jk9edb6s.default-release/cookies.sqlite`;
+        if (!fs.existsSync(file)) {
+            reject(new Error(`File ${file} does not exist`));
+            return;
+        }
+        if (process.env.DEBUG) {
+            console.log(`File ${file} exists, trying firefox`);
+        }
+        let sql;
+        sql = `SELECT value FROM moz_cookies`;
+        if (typeof name === 'string' || typeof domain === 'string') {
+            sql += ` WHERE `;
+            if (typeof name === 'string') {
+                sql += `name = '${name}'`;
+                if (typeof domain === 'string') {
+                    sql += ` AND `;
+                }
+            }
+            if (typeof domain === 'string') {
+                sql += `host LIKE '${domain}';`;
+            }
+        }
+        const command = `sqlite3 "${file}" "${sql}"`;
+        if (process.env.DEBUG) {
+            console.log(command);
+        }
+        exec(command, {encoding: 'binary', maxBuffer: 1024}, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (stderr) {
+                reject(error);
+                return;
+            }
+            let stdoutAsBuffer = stdout;
+            if (typeof stdoutAsBuffer === 'string' && stdoutAsBuffer.length > 0) {
+                // noinspection JSCheckFunctionSignatures
+                stdoutAsBuffer = Buffer.from(stdoutAsBuffer, 'binary').slice(0, -1);
+            }
+            if (stdoutAsBuffer && stdoutAsBuffer.length > 0) {
+                resolve(stdoutAsBuffer);
+            }
+        });
+    });
+}
 
-async function getEncryptedCookie(name, domain) {
+async function getEncryptedChromeCookie(name, domain) {
     if (name && typeof name !== 'string') {
         throw new Error('name must be a string');
     }
@@ -37,6 +97,10 @@ async function getEncryptedCookie(name, domain) {
     }
     return await new Promise((resolve, reject) => {
         const file = `${process.env.HOME}/Library/Application Support/Google/Chrome/Default/Cookies`;
+        if (!fs.existsSync(file)) {
+            reject(new Error(`File ${file} does not exist`));
+            return;
+        }
         let sql;
         sql = `SELECT encrypted_value FROM cookies`;
         if (typeof name === 'string' || typeof domain === 'string') {
@@ -115,7 +179,7 @@ async function decrypt(password, encryptedData) {
  * @param {string|undefined} domain
  * @returns {Promise<string>}
  */
-async function getDecryptedCookie({name, domain}) {
+async function getChromeCookie({name, domain}) {
     if (name && typeof name !== 'string') {
         throw new Error('name must be a string');
     }
@@ -123,25 +187,66 @@ async function getDecryptedCookie({name, domain}) {
         throw new Error('domain must be a string');
     }
     const password = await getChromePassword();
-    const encryptedData = await getEncryptedCookie(name, domain);
+    const encryptedData = await getEncryptedChromeCookie(name, domain);
+    console.log(encryptedData);
     return await decrypt(password, encryptedData);
 }
 
 // noinspection JSUnusedGlobalSymbols
 module.exports = {
-    getDecryptedCookie
+    getDecryptedCookie: getChromeCookie,
+    getChromeCookie
 };
 
 if (process.argv) {
     if (process.argv.length > 2) {
         const name = process.argv[2];
         const domain = process.argv[3];
-        getDecryptedCookie({name, domain})
-            .then((cookie) => {
-                if (typeof cookie === 'string' && cookie.length > 0) {
-                    console.log(cookie);
-                }
-            })
-            .catch(console.error);
+
+        if (process.argv.includes('--debug')) {
+            process.env.DEBUG = "true";
+        }
+        if (process.argv.includes('--verbose')) {
+            process.env.VERBOSE = "true";
+        }
+        if (process.argv.includes("--chrome-only")) {
+            process.env.CHROME_ONLY = "true";
+        }
+        if (process.argv.includes("--firefox-only")) {
+            process.env.FIREFOX_ONLY = "true";
+        }
+        if (process.argv.includes("--ignore-expired")) {
+            process.env.IGNORE_EXPIRED = "true";
+        }
+
+        if (process.env.CHROME_ONLY) {
+            getChromeCookie({name, domain})
+                .then(console.log)
+                .catch(console.error);
+        } else if (process.env.FIREFOX_ONLY) {
+            getFirefoxCookie({name, domain})
+                .then(console.log)
+                .catch(console.error);
+        } else {
+            getChromeCookie({name, domain})
+                .then(r => {
+                    if (typeof r === 'string') {
+                        console.log(r);
+                    } else {
+                        return getFirefoxCookie({name, domain}).catch(console.error);
+                    }
+                })
+                .catch((e) => {
+                    if (process.env.VERBOSE) {
+                        console.error(e);
+                    }
+                    return getFirefoxCookie({name, domain}).catch(console.error);
+                })
+                .then(r => {
+                    if (typeof r === 'string') {
+                        console.log(r);
+                    }
+                });
+        }
     }
 }

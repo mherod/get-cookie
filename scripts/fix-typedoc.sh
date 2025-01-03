07236c93141f7933cef4cf61232e7508661b214c
Matthew Herod <matthew.herod@gmail.com>
2025-01-03 06:54:03 +0000

    fix: make TypeDoc fix script Linux-compatible and handle more dead links

--- scripts/fix-typedoc.sh
+++ scripts/fix-typedoc.sh
@@ -4,4 +4,10 @@
 find docs/reference -name "README.md" -exec sh -c 'mv "$1" "$(dirname "$1")/index.md"' _ {} \;
 
 # Update all .md files to use the correct links
-find docs/reference -name "*.md" -exec sed -i '' 's/README.md/index.html/g' {} \;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS version
+  find docs/reference -name "*.md" -exec sed -i '' 's/README.md/index.html/g' {} \;
+else
+  # Linux version
+  find docs/reference -name "*.md" -exec sed -i 's/README.md/index.html/g' {} \;
+fi

7d088d375582aeee2e2dd2b2b13db851209d5bde
Matthew Herod <matthew.herod@gmail.com>
2025-01-03 16:33:46 +0000

    refactor: remove tough-cookie dependency and in-memory store

--- src/core/browsers/index.ts
+++ src/core/browsers/index.ts
@@ -2,4 +2,3 @@ export * from "./chrome/ChromeCookieQueryStrategy";
 export * from "./firefox/FirefoxCookieQueryStrategy";
 export * from "./safari/SafariCookieQueryStrategy";
 export * from "./CompositeCookieQueryStrategy";
-export * from "./CookieStoreQueryStrategy";

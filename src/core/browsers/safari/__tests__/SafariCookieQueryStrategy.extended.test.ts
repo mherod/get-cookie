1bc8149a92f7b2f02eaed72c4700584b3e45bb0d
Matthew Herod <matthew.herod@gmail.com>
2025-01-05 01:52:16 +0000

    test: remove empty test file

--- src/core/browsers/safari/__tests__/SafariCookieQueryStrategy.extended.test.ts
+++
@@ -1,5 +0,0 @@
-// This file has been split into separate test files:
-// - SafariCookieQueryStrategy.comments.test.ts
-// - SafariCookieQueryStrategy.port.test.ts
-// - SafariCookieQueryStrategy.version.test.ts
-// - SafariCookieQueryStrategy.undefined.test.ts
